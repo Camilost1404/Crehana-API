5506f4a6607679a0f4806b561b7146efbfad5d42
Andrés Torres <camitorres1404@gmail.com>
2025-06-21 02:07:37 -0500

    feat: add swagger config

---
+++ app/core/constants.py
@@ -0,0 +1,14 @@
+from app.core.settings import settings
+
+APP_NAME = settings.app_name
+TITLE = "Crehana API - Prueba Técnica"
+APP_VERSION = settings.app_version
+DESCRIPTION = """
+Esta aplicación es una prueba técnica realizada según lo especificado,
+mostrando una interfaz RESTful para acceder a contenido educativo, recursos y
+herramientas diseñadas para estudiantes y educadores.
+"""
+CONTACT = {"name": "Andres Camilo Torres Rosales", "email": "camitorres1404@gmail.com"}
+LICENSE = {"name": "MIT", "url": "https://opensource.org/license/mit/"}
+SWAGGER_UI_PARAMETERS = {"syntaxHighlight.theme": "obsidian"}
+SWAGGER_FAVICON_URL = "https://example.com/your-favicon.ico"
--- app/main.py
+++ app/main.py
@@ -1,8 +1,32 @@
+import logging
+
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
-app = FastAPI()
+from app.core.constants import (
+    APP_VERSION,
+    CONTACT,
+    DESCRIPTION,
+    LICENSE,
+    SWAGGER_FAVICON_URL,
+    SWAGGER_UI_PARAMETERS,
+    TITLE,
+)
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI(
+    title=TITLE,
+    version=APP_VERSION,
+    description=DESCRIPTION,
+    contact=CONTACT,
+    license_info=LICENSE,
+    swagger_ui_parameters=SWAGGER_UI_PARAMETERS,
+    swagger_favicon_url=SWAGGER_FAVICON_URL,
+)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", include_in_schema=False, response_class=RedirectResponse)
+async def redirect_to_swagger():
+    logger.info("Redirect to swagger...")
+    return RedirectResponse(url="/docs")
